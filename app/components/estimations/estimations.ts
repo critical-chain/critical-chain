d62122c625dfe498df3fe1fe10a6a34662f9f566
Kirill Pimenov <kpimenov@suse.de>
2016-01-20 13:37:36 +0100

    Estimations list

--- app/components/estimations/estimations.ts
+++ app/components/estimations/estimations.ts
@@ -1,8 +1,25 @@
 import {Component} from 'angular2/core';
+import {Estimation, EstimationService} from '../../services/estimations';
 
 @Component({
   selector: 'estimations',
-  templateUrl: './components/estimations/estimations.html'
+  templateUrl: './components/estimations/estimations.html',
+  providers: [EstimationService]
   //styleUrls: ['./components/home/home.css']
 })
-export class EstimationsCmp {}
+export class EstimationsCmp {
+  estimations: Estimation[];
+
+  newEstimationTitle: string;
+  newEstimationValue: string;
+
+  constructor(estimationService: EstimationService) {
+    this.estimations = estimationService.getEstimations();
+  };
+
+  createEstimation() {
+    this.estimations.push(new Estimation(this.newEstimationTitle, +this.newEstimationValue));
+    this.newEstimationTitle = '';
+    this.newEstimationValue = '';
+  };
+}
---
+++ app/services/estimations.ts
@@ -0,0 +1,20 @@
+export class Estimation {
+  constructor(
+    public title: string,
+    public value: number
+  ) { };
+}
+
+export class EstimationService {
+  values: Estimation[];
+
+  constructor() {
+    this.values = [
+      new Estimation('Sell!', 42)
+    ];
+  };
+
+  getEstimations() {
+    return this.values;
+  }
+}
