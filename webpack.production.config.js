ad6931c2c429b04d6368036f719c5bd5945a8e00
Kirill Pimenov <kirill@pimenov.cc>
2016-06-05 11:36:02 +0200

    Webpack config for production

---
+++ webpack.production.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.jsx'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ]
+};
