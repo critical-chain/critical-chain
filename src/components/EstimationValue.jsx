30e03923eb95afad6675a9180aff58a24c4da36e
Kirill Pimenov <kirill@pimenov.cc>
2016-06-09 11:07:45 +0200

    Ability to render values for empty estimations

--- src/components/EstimationValue.jsx
+++ src/components/EstimationValue.jsx
@@ -6,8 +6,12 @@ export default class EstimationValue extends React.Component {
     var values = this.props.estimation.get('steps', List([])).map((step) => {
       return step.get('value')
     });
-    var sum = values.reduce((a, b) => a + b);
-    var buffer = (sum / Math.sqrt(values.size));
+    var sum = 0;
+    var buffer = 0;
+    if(values.size > 0) {
+      sum = values.reduce((a, b) => a + b);
+      buffer = (sum / Math.sqrt(values.size));
+    }
     return <div className="estimationValue">
       <span className="base">{sum.toFixed(1)}</span>
       <span className="small">+</span>
