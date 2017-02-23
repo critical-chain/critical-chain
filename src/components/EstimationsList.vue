e9d297d9d80895d86ecf26f946c921ee51478c2e
Kirill Pimenov <kirill@pimenov.cc>
2017-02-23 15:54:39 +0100

    Fancy EstimationsList

--- src/components/EstimationsList.vue
+++ src/components/EstimationsList.vue
@@ -1,31 +1,50 @@
 <template>
   <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="1">
+        CriticalChain Estimator
+      </q-toolbar-title>
+    </div>
+
     <div class="layout-view">
-      <div class="blankslate" v-if="empty">
-        <h3>This is a blank slate</h3>
-        <p>Use it to provide information when no dynamic content exists.</p>
-      </div>
+      <div class="layout-padding">
+        <div class="row justify-center">
+          <div class="width-4of5 gt-md-width-3of5">
+            <div class="card">
+              <div class="blankslate" v-if="empty">
+                <h3>This is a blank slate</h3>
+                <p>Use it to provide information when no dynamic content exists.</p>
+              </div>
 
-      <div class="list highlight item-delimiter" v-else>
-        <div class="item" v-for="estimation in estimations">
-          <router-link :to="{ name: 'estimation', params: {id: estimation.id}}" tag="div" class="item-content">
-            {{estimation.title}}
-          </router-link>
+              <div class="list highlight item-delimiter" v-else>
+                <div class="item" v-for="estimation in estimations">
+                  <router-link :to="{ name: 'estimation', params: {id: estimation.id}}" tag="div" class="item-content">
+                    {{estimation.title}}
+                  </router-link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div class="row justify-end">
-        <div class="stacked-label">
-          <input autofocus v-focus.lazy="true"
-                 v-model="newEstimationTitle" @keyup.enter="addEstimation(newEstimationTitle)">
-          <label>New estimation title</label>
+        <div class="reverse-row">
+          <div class="width-1of5 gt-md"></div>
+          <div class="row no-wrap">
+            <div class="stacked-label self-stretch">
+              <input autofocus v-focus.lazy="true"
+                     v-model="newEstimationTitle" @keyup.enter="addEstimation(newEstimationTitle)">
+              <label>New estimation title</label>
+            </div>
+            <div>
+              <button class="primary circular"
+                      @click="addEstimation(newEstimationTitle)"
+                      :disabled="addDisabled"
+                      title="Add estimation">
+                <i>add</i>
+              </button>
+            </div>
+          </div>
         </div>
-        <button class="primary small circular"
-                @click="addEstimation(newEstimationTitle)"
-                :disabled="addDisabled"
-                title="Add estimation">
-          <i>add</i>
-        </button>
       </div>
     </div>
   </q-layout>
